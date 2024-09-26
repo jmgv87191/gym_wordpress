041fff7f389c2cfcad6aacdb57767e6d32ba5d13
juan manuel <jmgv87191@gmail.com>
2024-09-26 10:08:52 -0700

    as

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -10,5 +10,11 @@
 
     add_action('init','gymfitness_menus');
 
+    function gymfitness_scripts_styles(){
+        wp_enqueue_style( 'style', get_stylesheet_uri(),array(),'1.0.0' );
+    }
+
+    add_action('wp_enqueue_scripts','gymfitness_scripts_styles');
+
 
 ?>
