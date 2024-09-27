084997c39bb2786a73dff67970031a6cb390f3cb
juan manuel <jmgv87191@gmail.com>
2024-09-27 10:33:21 -0700

    asd

--- wp-content/themes/gymfitness/footer.php
+++ wp-content/themes/gymfitness/footer.php
@@ -3,16 +3,19 @@
     <hr>
 
     <div class="contenido-footer">
-    <?php
+        <?php
 
-$args = array(
-    'theme_location'=>'menu-principal',
-    'container' =>'nav',
-    'container_class' => 'menu-principal'
-);
+        $args = array(
+            'theme_location'=>'menu-principal',
+            'container' =>'nav',
+            'container_class' => 'menu-principal'
+        );
 
-    wp_nav_menu($args);
-?>
+            wp_nav_menu($args);
+        ?>
+
+        <p class="copyright" >Todos los derechos reservados. <?php echo get_bloginfo('name')." ". date('Y') ?> </p>
+
     </div>
 
 </footer>
